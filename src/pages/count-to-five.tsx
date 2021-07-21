07b71606b1631ada5d69b91312dea06496811702
mrkutly <mark.sauer.utley@gmail.com>
2021-07-21 12:57:34 -0500

    test netlify redirect

---
+++ src/pages/count-to-five.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const CountToFivePage = () => (
+	<Layout>
+		<SEO title="count to five" />
+		<main>hello there</main>
+	</Layout>
+)
+
+export default CountToFivePage
